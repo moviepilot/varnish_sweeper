cc7062bb9f50ff70aa9e4e2b660b1dd4e92b917d
Daniel Bornkessel <daniel@moviepilot.com>
2011-01-10 19:32:29 +0100

    don't auto-include ... it makes some trouble

--- lib/varnish_sweeper.rb
+++ lib/varnish_sweeper.rb
@@ -1,4 +1,2 @@
 require 'varnish'
-ApplicationController.send(:include, Varnish)
-ActionController::Caching::Sweeper.send(:include, Varnish)
 require 'sweeper_job'
--- spec/varnish_sweeper_spec.rb
+++ spec/varnish_sweeper_spec.rb
@@ -1,15 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "VarnishSweeper" do
-  it "should provide our functions to the ApplicationController" do
-    ApplicationController.new.should respond_to(:make_cacheable)
-    ApplicationController.new.should respond_to(:sweep_cache_for)
-  end
-
-  it "should provide our functions to the ActionController::Caching::Sweeper" do
-    ActionController::Caching::Sweeper.new.should respond_to(:make_cacheable)
-    ActionController::Caching::Sweeper.new.should respond_to(:sweep_cache_for)
-  end
+#  it "should provide our functions to the ApplicationController" do
+#    ApplicationController.new.should respond_to(:make_cacheable)
+#    ApplicationController.new.should respond_to(:sweep_cache_for)
+#  end
+#
+#  it "should provide our functions to the ActionController::Caching::Sweeper" do
+#    ActionController::Caching::Sweeper.new.should respond_to(:make_cacheable)
+#    ActionController::Caching::Sweeper.new.should respond_to(:sweep_cache_for)
+#  end
 
   it "should make the sweeper job available" do
     defined?(SweeperJob).should_not be_nil
